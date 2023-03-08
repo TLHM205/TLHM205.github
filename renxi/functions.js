8ff3c6aad2e5c28ccad4ea5a0047ab147aa0da0b
TLHM205 <117747723+TLHM205@users.noreply.github.com>
2023-03-08 08:49:57 +0700

    Update functions.js

--- renxi/functions.js
+++ renxi/functions.js
@@ -35,14 +35,14 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 100;
-	var days = Math.floor(seconds / (360 * 24));
-	seconds = seconds % (360 * 24);
-	var hours = Math.floor(seconds / 360);
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % 360;
+	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
